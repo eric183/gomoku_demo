85df1db98342b9630b89efaa06e43492d0a95dc1
ericKuang <297466058@qq.com>
2017-08-04 18:04:26 +0800

    save

--- src/gomoku.js
+++ src/gomoku.js
@@ -1,10 +1,15 @@
+var config = {};
+
 function query(dom) {
-    return document.querySelector(dom);
+    dom = document.querySelectorAll(dom);
+    return dom.length == 1 ? dom[0] : dom;
 }
 
-function querys(doms) {
-    return document.querySelectorAll(doms);
-}
+/**
+ * 类数组转换为数 
+ * @param {any} array_like 
+ * @returns 
+ */
 
 function toArray(array_like) {
     return Array.prototype.slice.call(array_like);
@@ -17,25 +22,56 @@ function _extends(_from, _target) {
     return _target;
 }
 
-
-function Gomoku() {}
-
-
-
-console.log(query('#root'));
-var _template = "";
-var _t = [];
-
-var li_count = [1, 2, 3, 4, 5];
-
-
-function insertChildren() {
-    for (let i = 0; i < li_count.length; i++) {
-        _template = _template + `<li>${li_count[i]}</li>`;
-    };
-    return _template;
+function Gomoku(object) {
+    // Gomoku.fn = 2323;
+    object.el = query(object.el);
+    // object.el = query("root");
+    fakeConstructor(object);
 }
 
+/**
+ * constructor不可靠，这里做一个fakeConstructor来替代
+ * @param {Object} configObj 
+ */
+
+function fakeConstructor(configObj) {
+    _extends(configObj, config);
+}
+
+// Gomoku.prototype.constructor = {
+
+// }
+var _template = '';
+Gomoku.prototype.initStage = function() {
+    // console.log(this.name);
+    config.el.style.height = `${config.el.clientWidth}px`;
+
+    toArray(query('ul')).forEach((i) => {
+        i.insertAdjacentHTML('beforeend', config.template);
+    })
+
+    return this;
+}
+
+{
+    function insertChildren() {
+        for (let i = 0; i < 15; i++) {
+            _template = _template + `<li></li>`;
+        };
+        return _template;
+    }
+
+
+    config = _extends({ template: insertChildren() }, config);
+
+}
+
+
+var m = new Gomoku({ el: "#root" }).initStage();
 // insertChildren();
-console.log(`<ul>${insertChildren()}</ul>`);
+// console.log();
 // console.log(_template);
+
+root.addEventListener('click', function(e) {
+    console.log(e);
+}, false)
